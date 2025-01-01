934196e20e3a82ac40a9c967be01691adb906911
daniperdomo <danielperdomomago@gmail.com>
2025-01-01 14:44:34 -0400

    Base del home (ods's version queeee)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Header from '../components/header'; // Asegúrate de que la ruta sea correcta
-import Footer from '../components/footer'; // Asegúrate de que la ruta sea correcta
+import Header from '../components/header';
+import Footer from '../components/footer';
+import '../styles/estiloHome.css';
 
 const Home = () => {
     const { title } = useParams();
@@ -9,9 +10,21 @@ const Home = () => {
     return (
         <>
             <Header />
-            <h1 className="text-4xl font-bold mb-4">
-                Heyyy olvidona
-            </h1>
+            <div className="home-container">
+                <h1 className="home-title">Bienvenido al Sistema de Gestión de Tesis</h1>
+                <p className="home-paragraph">
+                    En un mundo donde los Objetivos de Desarrollo Sostenible (ODS) son cada vez más relevantes, 
+                    la implementación de un sistema de gestión de tesis puede contribuir significativamente al cuidado del medio ambiente. 
+                    Este sistema fomenta la sostenibilidad al reducir el uso de papel, digitalizando procesos que anteriormente dependían 
+                    de documentos físicos. Esto no solo minimiza el impacto ambiental, sino que también promueve la eficiencia y la accesibilidad.
+                </p>
+                <p className="home-paragraph">
+                    Además, al adoptar soluciones tecnológicas, las instituciones educativas pueden alinearse con los ODS, 
+                    específicamente con el objetivo 12: "Producción y consumo responsables". Este sistema de gestión no solo beneficia 
+                    al medio ambiente, sino que también mejora la experiencia de los estudiantes y profesores al centralizar la información, 
+                    optimizar la comunicación y facilitar el seguimiento de proyectos académicos.
+                </p>
+            </div>
             <Footer />
         </>
     );
